60dae21ef7c77963f8098926a35b868e5d6a5dd4
Elin <51740831+EllVEBIT@users.noreply.github.com>
2020-09-27 22:25:47 +0500

    Update futaba.py

--- futaba.py
+++ futaba.py
@@ -1,3 +1,4 @@
+
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
@@ -11,10 +12,15 @@ import re
 import wikipedia
 import waifu
 import hello
+import nekos
+import json
+import io
+import meme
 import asyncio
 import json
 from time import strftime
 from bs4 import BeautifulSoup
+from discord.ext.commands import has_permissions, CheckFailure
 
 client = discord.Client()
 
@@ -26,15 +32,28 @@ async def on_ready():
 
     await client.change_presence( status = discord.Status.online, activity = discord.Game('f!main') )
 
-# ошибки
+# ошибки и собшения
 
 @client.event
 async def on_command_error(ctx, error):
   if isinstance(error, commands.CommandNotFound):
-    await ctx.send('Такой команды нет, чтобы посмотреть список доступных команд введите f!main')
+    await ctx.send('Такой команды нет, либо команда написанна неправильно. Чтобы посмотреть список доступных команд введите "f!main" ')
     
   if isinstance(error, commands.MissingPermissions):
     await ctx.send('Данная команда доступна только Администраторам!')
+
+# создатель
+
+@client.command()
+async def creator(ctx):
+  author = ctx.message.author
+  embed = discord.Embed(color=0xff80ff)
+  embed.set_author(name="Информация")
+  embed.add_field(name="Создатель:", value='Elin#6696', inline=False)
+  embed.add_field(name="Исходный код", value='https://github.com/EllVEBIT/Futaba', inline=False)
+  embed.add_field(name="Контакты", value='https://vk.com/evilbitsd', inline=False)
+  
+  await ctx.send(embed=embed)
 
 # меню
 
@@ -76,29 +95,31 @@ async def clear(ctx,amount=2000):
   author = ctx.message.author
   await ctx.send(f'{ author.mention} удалил(а) несколько сообшений.')
 
-# твоя вайфу из серий Fate
+# пинг
 
 @client.command()
-async def fate(ctx):
-  chosen_index = random.randint(0, 409 )
-  
-  embed = discord.Embed(title=waifu.waifuName[chosen_index], description=waifu.waifuSeries[chosen_index], timestamp=datetime.datetime.utcnow(), color=0xff80ff)
-  embed.set_image(url=waifu.waifuLinks[chosen_index])
-  embed.set_footer(text=f"Попросил(а): {ctx.author.name}")
-  
-  await ctx.send(content=None, embed=embed)
+async def ping(ctx):
+  await ctx.send(f"Пинг {round(client.latency * 1000)}ms")
 
 # приветсвие для участников
 
 @client.event
 async def on_member_join(member):
-  hi = random.randint(0, 1 )
-  
-  channel = client.get_channel(751126851916660736)
-  embed=discord.Embed(title="", description=f'{member.mention} Добро пожаловать!', color=0xff80ff)
-  embed.set_image(url=hello.gifs[hi])
-  
-  await channel.send(embed=embed, content=None)
+    print(f'{member} has joined')
+    embed = discord.Embed(
+        title='Hello there!',
+        description=f'thanks for joining {member}! have a good time,\
+        and dont forget to follow the rules! to be able to chat, \
+        please type ```+verify``` in the #verify-me channel',
+        colour=discord.Colour.blurple()
+    )
+    channel = discord.utils.get(member.guild.channels, name="┊❀log")
+    bruh = member.avatar_url
+
+    embed.set_image(url=bruh)
+
+    await member.send(embed=embed)
+    await channel.send(embed=embed)
 
 # wiki
 
@@ -128,13 +149,132 @@ async def mem(ctx):
  
   await ctx.send(embed=embed, content=None)
 
-# написать от лица бота (только для админов)
+# написать от лица бота (только дя админов)
 
 @client.command()
 @commands.has_permissions(administrator = True)
 async def say(ctx, *, msg):
   await ctx.message.delete()
   await ctx.send("{}" .format(msg))
+
+#Fun
+###########################################
+# neko_gif
+
+@client.command()
+async def neko_gif(ctx):
+    embed = discord.Embed(
+        title='',
+        description='',
+        timestamp=datetime.datetime.utcnow(), color=0xff80ff)
+    ngif = nekos.img("ngif")
+    embed.set_image(url=ngif)
+    embed.set_footer(text=f"Попросил(а): {ctx.author.name}")
+
+    await ctx.send(embed=embed)
+
+# твоя вайфу из серий Fate
+
+@client.command()
+async def fate(ctx):
+  chosen_index = random.randint(0, 409 )
   
+  embed = discord.Embed(title=waifu.waifuName[chosen_index], description=waifu.waifuSeries[chosen_index], timestamp=datetime.datetime.utcnow(), color=0xff80ff)
+  embed.set_image(url=waifu.waifuLinks[chosen_index])
+  embed.set_footer(text=f"Попросил(а): {ctx.author.name}")
+  
+  await ctx.send(content=None, embed=embed)
+
+# kiss
+
+@client.command()
+async def kiss(ctx, member: discord.Member, *, reason=""):
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    embed = discord.Embed(
+        title=f"{ctx.message.author} Поцеловал(а) {member.name} {reason}",
+        description='',
+        colour=discord.Colour.from_rgb(r, g, b)
+    )
+    kiss = nekos.img("kiss")
+
+    embed.set_image(url=kiss)
+
+    await ctx.send(embed=embed)
+
+@client.command()
+async def feed(ctx):
+    embed = discord.Embed(
+        title='',
+        description='',
+        timestamp=datetime.datetime.utcnow(), color=0xff80ff)
+    feed = nekos.img("feed")
+    embed.set_image(url=feed)
+    embed.set_footer(text=f"Попросил(а): {ctx.author.name}")
+
+    await ctx.send(embed=embed)
+
+#Nsfw 
+###########################################
+# erokemo
+
+@client.command()
+async def erokemo(ctx):
+    if not ctx.channel.is_nsfw():
+      author = ctx.message.author
+      await ctx.send("Такие штучки работают только в NSFW каналах!")
+      sys.stderr = object
+    if ctx.channel.is_nsfw():
+        embed = discord.Embed(
+            title='',
+            description='',
+            timestamp=datetime.datetime.utcnow(), color=0xff80ff
+        )
+    erokemo = nekos.img("erokemo")
+    embed.set_image(url=erokemo)
+    embed.set_footer(text=f"Попросил(а): {ctx.author.name}")
+
+    await ctx.send(embed=embed)
+
+# walpapper
+
+@client.command()
+async def wallpaper(ctx):
+    if not ctx.channel.is_nsfw():
+      author = ctx.message.author
+      await ctx.send("Такие штучки работают только в NSFW каналах!")
+      sys.stderr = object
+    if ctx.channel.is_nsfw():
+        embed = discord.Embed(
+            title='',
+            description='',
+            timestamp=datetime.datetime.utcnow(), color=0xff80ff)
+    wallpaper = nekos.img("wallpaper")
+    embed.set_image(url=wallpaper)
+    embed.set_footer(text=f"Попросил(а): {ctx.author.name}")
+    
+    await ctx.send(embed=embed)
+
+# cum
+
+@client.command()
+async def cum(ctx):
+    if not ctx.channel.is_nsfw():
+      author = ctx.message.author
+      await ctx.send("Такие штучки работают только в NSFW каналах!")
+      sys.stderr = object
+    if ctx.channel.is_nsfw():
+        embed = discord.Embed(
+            title='',
+            description='',
+            timestamp=datetime.datetime.utcnow(), color=0xff80ff)
+    cum = nekos.img("cum")
+
+    embed.set_image(url=cum)
+    embed.set_footer(text=f"Попросил(а): {ctx.author.name}")
+    
+    await ctx.send(embed=embed)
+
 # Твой токен
 client.run('токен')
