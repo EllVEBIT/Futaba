47c9c782e2c4d6459a3e4b15ab5fe0cfe5d2273b
Elin <51740831+EllVEBIT@users.noreply.github.com>
2020-10-09 09:31:38 +0500

    Update futaba.py

--- futaba.py
+++ futaba.py
@@ -14,6 +14,7 @@ import waifu
 import nekos
 import json
 import io
+from random import choice
 from time import strftime
 from bs4 import BeautifulSoup
 from discord.ext.commands import has_permissions, CheckFailure
