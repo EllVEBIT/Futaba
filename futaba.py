c18c82e7632def63e0a0fa5c98471dace978585d
Elin <51740831+EllVEBIT@users.noreply.github.com>
2020-10-09 11:44:51 +0500

    Update futaba.py

--- futaba.py
+++ futaba.py
@@ -30,7 +30,7 @@ async def on_ready():
 
 @tasks.loop(seconds=10)
 async def change_status():
-    await client.change_presence(activity=discord.Game(choice(status)))
+    await client.change_presence(activity=discord.Streaming(choice(status)))
 
 # ошибки и сообшения
 @client.event
