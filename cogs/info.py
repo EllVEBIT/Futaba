df7a2f4b39a2aaad6ffa63d103964e2023b72cca
Elin <51740831+EllVEBIT@users.noreply.github.com>
2021-02-03 21:22:09 +0500

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -1,4 +1,5 @@
 import discord
+import platform
 import os
 import psutil
 import time
@@ -6,6 +7,7 @@ import datetime
 from discord.ext import commands
 
 start_time = time.time()
+
 
 class Info(commands.Cog):
     def __init__(self, bot):
@@ -30,34 +32,65 @@ class Info(commands.Cog):
         msg = '**{0}** сек{1}.'.format(int(s), '' if m == 0 else '')
       if m != 0:
           e = '' if h == 0 else '.'
-          msg = ' : **{0}** мин : '.format(int(m)) + msg.replace('.', '') + e
+          msg = '  **{0}** мин  '.format(int(m)) + msg.replace('.', '') + e
       if h != 0:
         e = '' if d == 0 else '.'
-        msg = ' : **{0}** час : '.format(int(h)) + msg.replace('.', '') + e
+        msg = '  **{0}** час  '.format(int(h)) + msg.replace('.', '') + e
       if d != 0:
         e = '' if w == 0 else '.'
-        msg = ' : **{0}** день '.format(int(d)) + msg.replace('.', '').replace('', '') + e
+        msg = '  **{0}** день '.format(int(d)) + msg.replace('.', '').replace('', '') + e
       if w != 0:
-        msg = ' : **{0}** год {1}'.format(int(w)) + msg.replace('.', '') + ''
+        msg = '  **{0}** год {1}'.format(int(w)) + msg.replace('.', '') + ''
       if m == 0:
         msg = ' '+msg
       else:
         msg = msg[2:]
       
       RAM = psutil.Process(os.getpid()).memory_full_info().rss / 1024**2
+      
+      TRAM =psutil.virtual_memory().total / 1024**2
+      
       ping = round(self.bot.latency * 1000, 1)
       
       embed = discord.Embed(title='Статус',color=discord.Color.dark_red())
-      embed = embed.add_field(name="Время работы", value= f'Бот Online {msg}' ,inline=False)
-      embed = embed.add_field(name="Использованно", value=f"{RAM:.2f} MB" ,inline=False)
-      embed = embed.add_field(name= "Пинг", value= f"{int(ping)}ms",inline=False)
+      embed.add_field(name="Время работы", value= f'{msg}' ,inline=False)
+      embed.add_field(name="Система", value=f"{platform.system()}",inline=False)
+      embed.add_field(name="Ядер", value=f"{psutil.cpu_count()}",inline=True)
+      embed.add_field(name="Используется", value=f"{RAM:.2f} MB" ,inline=True)
+      embed.add_field(name="Всего", value=f"{TRAM:.2f} MB" ,inline=True)
+      embed.add_field(name= "Пинг", value= f"{int(ping)} ms",inline=False)
       await ctx.send(embed = embed)
     
     @commands.command()
     async def invite(self,ctx):
       embed = discord.Embed(color=discord.Color.dark_red())
-      embed.add_field(name="Добавить меня к себе можно по этой ссылке", value='ссылка', inline=False)
+      embed.add_field(name="Добавить меня к себе можно по этой ссылке", value='https://discord.com/oauth2/authorize?client_id=737318737887232111&scope=bot&permissions=8', inline=False)
       await ctx.send( embed = embed )
+    
+    @commands.command()
+    async def info(self,ctx, *users:discord.Member):
+      if len(users) == 0:
+        users = [ctx.message.author]
+      for user in users:
+        emb = discord.Embed(title='Информация о пользователе',color=discord.Color.dark_red())
+        emb.add_field(name="Когда присоединился:",value=user.joined_at,inline=False)
+        emb.add_field(name="Никнейм:",value=user.display_name,inline=False)
+        emb.add_field(name="ID",value=user.id,inline= False)
+        emb.add_field(name="Аккаунт был создан:",value=user.created_at.strftime("%#d %B %Y  %I:%M %p"),inline=False)
+        roles = []
+        for role in user.roles:
+          if role.name != '@everyone':
+            roles.append(role.mention)
+        
+        if len(roles) > 0:
+          roles = ' , '.join(roles)
+        else:
+          roles = "Пользователь не имеет никаких ролей"
+        
+        emb.add_field(name="Роль", value=roles)
+        emb.set_thumbnail(url=user.avatar_url)
+        await ctx.send(embed = emb)
+
 
 def setup(bot):
   bot.add_cog(Info(bot))
